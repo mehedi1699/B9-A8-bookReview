3b4debd34b76ba5ef78f1841f85c07eb6d65e2a6
mehedi1699 <mehedi1699.com>
2024-05-21 03:38:08 +0600

    graph section done

--- src/components/PagesRead/PagesRead.jsx
+++ src/components/PagesRead/PagesRead.jsx
@@ -1,11 +1,22 @@
+import { BarChart, Bar,XAxis,YAxis,Tooltip,Legend,CartesianGrid } from 'recharts';
+import { getReadListBooks } from '../Utility/localStorage';
 
 
 const PagesRead = () => {
+    const readList = getReadListBooks()
+    const data= readList;
     return (
-        <div>
-            <h1>This is Pages to Read</h1>
+        <div className={`bg-gray-100 p-12 text-center rounded-3xl mx-auto ${readList.length <= 0 && 'hidden'}`}>
+            <BarChart className='mx-auto' width={730} height={450} data={data}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="bookName" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="totalPages" fill="#1884d8" />
+            </BarChart>
         </div>
     );
 };
 
-export default PagesRead;<h1>This is Pages to Read</h1>
+export default PagesRead;
